0f3b0d527f855a8b47a51f74a0e0c296d03f24aa
Amit Kumar <amit@coding.py>
2024-03-29 20:25:46 +0530

    fix-keyboard-opening

--- app/ui/typing/WordsContainer.tsx
+++ app/ui/typing/WordsContainer.tsx
@@ -1,6 +1,9 @@
 const WordsContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-secondary-2 text-stone-400 p-8 w-11/12 rounded-lg shadow-md  border-gray-300 ">
+    <div
+      className="bg-secondary-2 text-stone-400 p-8 w-11/12 rounded-lg shadow-md  border-gray-300 "
+      tabIndex={0}
+    >
       <div className="relative text-2xl tracking-wider max-w-5xl leading-relaxed break-all   ">
         {children}
       </div>

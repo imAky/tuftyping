07d55ef2bd4b23a6730607c42c4766e747728c62
Amit Kumar <amit@coding.py>
2024-03-29 21:05:17 +0530

    fixed keyboard opening

--- app/ui/typing/WordsContainer.tsx
+++ app/ui/typing/WordsContainer.tsx
@@ -1,19 +1,22 @@
 import { useRef } from "react";
 
 const WordsContainer = ({ children }: { children: React.ReactNode }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   const handleFocus = () => {
-    containerRef.current?.focus();
+    inputRef.current?.focus();
   };
 
   return (
     <div
-      className="bg-secondary-2 text-stone-400 p-8 w-11/12 rounded-lg shadow-md  border-gray-300 outline-none focus:ring-0"
-      ref={containerRef}
+      className="bg-secondary-2 text-stone-400 p-8 w-11/12 rounded-lg shadow-md border-gray-300 outline-none focus:ring-0"
       tabIndex={0}
       onClick={handleFocus}
     >
-      <div className="relative text-2xl tracking-wider max-w-5xl leading-relaxed break-all   ">
+      <input
+        className="w-full h-full absolute inset-0 opacity-0 cursor-text"
+        ref={inputRef}
+      />
+      <div className="relative text-2xl tracking-wider max-w-5xl leading-relaxed break-all">
         {children}
       </div>
     </div>

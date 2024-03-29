5b780eabc097e8102a0001d348462a6b5221ef72
Amit Kumar <amit@coding.py>
2024-03-29 20:47:09 +0530

    fixed-outline

--- app/ui/typing/WordsContainer.tsx
+++ app/ui/typing/WordsContainer.tsx
@@ -8,9 +8,8 @@ const WordsContainer = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div
-      className="bg-secondary-2 text-stone-400 p-8 w-11/12 rounded-lg shadow-md  border-gray-300 "
+      className="bg-secondary-2 text-stone-400 p-8 w-11/12 rounded-lg shadow-md  border-gray-300 outline-none focus:ring-0"
       tabIndex={0}
-      contentEditable
       onClick={handleFocus}
     >
       <div className="relative text-2xl tracking-wider max-w-5xl leading-relaxed break-all   ">

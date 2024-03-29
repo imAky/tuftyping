a3bcd29705ef6b71e3ee536f6b8743be3eb55a3b
Amit Kumar <amit@coding.py>
2024-03-29 20:41:46 +0530

    fixed-keyboard-focus

--- app/ui/typing/WordsContainer.tsx
+++ app/ui/typing/WordsContainer.tsx
@@ -1,8 +1,17 @@
+import { useRef } from "react";
+
 const WordsContainer = ({ children }: { children: React.ReactNode }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const handleFocus = () => {
+    containerRef.current?.focus();
+  };
+
   return (
     <div
       className="bg-secondary-2 text-stone-400 p-8 w-11/12 rounded-lg shadow-md  border-gray-300 "
       tabIndex={0}
+      contentEditable
+      onClick={handleFocus}
     >
       <div className="relative text-2xl tracking-wider max-w-5xl leading-relaxed break-all   ">
         {children}

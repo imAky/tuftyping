d993281e8105a53506224128fc01330fc34de40c
Amit Kumar <amit@coding.py>
2024-03-30 13:47:28 +0530

    working-input-focus-blur

--- app/ui/typing/WordsContainer.tsx
+++ app/ui/typing/WordsContainer.tsx
@@ -1,5 +1,16 @@
 import React, { KeyboardEventHandler, useRef, useEffect } from "react";
 
+// Define the isMobile function outside of the component
+const isMobile = () => {
+  if (typeof window !== "undefined") {
+    const userAgent = navigator.userAgent;
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      userAgent
+    );
+  }
+  return false; // Default to false if window or navigator is not available
+};
+
 const WordsContainer = ({
   children,
   keydownHandler,
@@ -9,16 +20,17 @@ const WordsContainer = ({
 }) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
-  // Focus the input element when the component mounts or updates
+  // Focus the input element when the component mounts or updates,
+  // but only if it's not a mobile device
   useEffect(() => {
-    if (inputRef.current) {
+    if (!isMobile() && inputRef.current) {
       inputRef.current.focus();
     }
   }, []);
 
-  // Handle blur event to prevent input from losing focus
-  const handleBlur = () => {
-    if (inputRef.current) {
+  // Function to handle click on the container
+  const handleContainerClick = () => {
+    if (isMobile() && inputRef.current) {
       inputRef.current.focus();
     }
   };
@@ -27,16 +39,24 @@ const WordsContainer = ({
     keydownHandler(event);
   };
 
+  // Function to handle blur event on the input
+  const handleBlur = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div
       className="relative bg-secondary-2 text-stone-400 p-8 w-11/12 rounded-lg shadow-md border-gray-300 outline-none focus:ring-0"
       tabIndex={0}
-      onBlur={handleBlur} // Prevent input from losing focus
+      onClick={handleContainerClick} // Always attach handleContainerClick
     >
       <input
         className="w-full h-full absolute inset-0 opacity-0 cursor-text"
         ref={inputRef}
         onKeyDown={handleKeydown}
+        onBlur={handleBlur} // Handle blur event
       />
       <div className="relative text-2xl tracking-wider max-w-5xl leading-relaxed break-all">
         {children}
